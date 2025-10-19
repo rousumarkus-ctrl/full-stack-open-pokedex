fad7787f798ed97daf02a3f48764076214a093a2
Markus Rousu <rousu.markus@gmail.com>
2025-10-19 14:14:36 +0300

    cron

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  throw 'error....'
-  // eslint-disable-next-line no-unreachable
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error....'
   res.send('1')
 })
 
